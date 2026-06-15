b292fe57bb74780cf4b9bcef07fd69e972bd767b
IamShivanshCoder <shivanshbansal0308@gmail.com>
2026-06-15 07:58:15 +0530

    add bufferline statusline and smear cursor ,enhance ui

--- lua/plugins/color.lua
+++
@@ -1,1 +0,0 @@
-vim.pack.add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,7 @@
+vim.pack.add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
+vim.pack.add({{src = "https://github.com/sphamba/smear-cursor.nvim"}})
+
+require('smear_cursor').setup({
+
+})
+
